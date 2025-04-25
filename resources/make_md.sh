59357bf8d16e40f6628660e539df4b4562e6b17a
step- <step-@users.noreply.github.com>
2025-04-25 15:34:12 +0200

    fix(build): clang-cpp does not allow "###" substitution

--- resources/make_md.sh
+++ resources/make_md.sh
@@ -17,5 +17,5 @@ exec 1> "$2"
 	versionh=../src/mtxversion.h               # Makefile
 
 cpp $SUB_CPPFLAGS -include $versionh \
-	-DATX3="###" -DHBRK="\\" -traditional -P "$1" | \
-	awk 'NF {p=1} p'
+	-DHBRK="\\" -traditional -P "$1" | \
+	awk '{sub(/ATX3/, "###")} NF {p=1} p'
